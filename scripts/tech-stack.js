0643c3cb6a480d8f491cebeafa2c5885299510d0
Allison Golden <141694129+AllisonG399@users.noreply.github.com>
2026-08-13 13:07:16 -0500

    Add tech stack configuration in tech-stack.js

---
+++ scripts/tech-stack.js
@@ -0,0 +1,149 @@
+const techStack = {
+  languages: [
+    {
+      name: "Java",
+      icon: "Java",
+    },
+    {
+      name: "Kotlin",
+      icon: "Kotlin",
+    },
+    {
+      name: "JavaScript",
+      icon: "JavaScript",
+    },
+    {
+      name: "TypeScript",
+      icon: "TypeScript",
+    },
+    {
+      name: "HTML5",
+      icon: "HTML5",
+    },
+    {
+      name: "CSS3",
+      icon: "CSS3",
+    },
+    {
+      name: "XML",
+      icon: "XML",
+    },
+  ],
+
+  web: [
+    {
+      name: "React",
+      icon: "React",
+    },
+    {
+      name: "Next.js",
+      icon: "Next.js",
+    },
+    {
+      name: "Vite",
+      icon: "Vite.js",
+    },
+    {
+      name: "Tailwind CSS",
+      icon: "Tailwind CSS",
+    },
+    {
+      name: "Bootstrap",
+      icon: "Bootstrap",
+    },
+    {
+      name: "Framer Motion",
+      icon: "framer-motion",
+    },
+  ],
+
+  android: [
+    {
+      name: "Android Studio",
+      icon: "Android Studio",
+    },
+    {
+      name: "Jetpack Compose",
+      icon: "jetpackcompose-original",
+    },
+    {
+      name: "Room",
+      icon: "room",
+    },
+    {
+      name: "SQLite",
+      icon: "SQLite",
+    },
+    {
+      name: "Kotlin Coroutines",
+      icon: "",
+    },
+    {
+      name: "MVVM",
+      icon: "",
+    },
+  ],
+
+  backend: [
+    {
+      name: "Node.js",
+      icon: "Node.js",
+    },
+    {
+      name: "RESTful APIs",
+      icon: "",
+    },
+    {
+      name: "JSON",
+      icon: "JSON",
+    },
+  ],
+
+  databases: [
+    {
+      name: "MySQL",
+      icon: "MySQL",
+    },
+    {
+      name: "MongoDB",
+      icon: "MongoDB",
+    },
+    {
+      name: "SQLite",
+      icon: "SQLite",
+    },
+  ],
+
+  tools: [
+    {
+      name: "Git",
+      icon: "Git",
+    },
+    {
+      name: "GitHub",
+      icon: "GitHub",
+    },
+    {
+      name: "GitHub Actions",
+      icon: "GitHub Actions",
+    },
+    {
+      name: "VS Code",
+      icon: "Visual Studio Code (VS Code)",
+    },
+    {
+      name: "IntelliJ IDEA",
+      icon: "IntelliJ IDEA",
+    },
+    {
+      name: "Vercel",
+      icon: "Vercel",
+    },
+    {
+      name: "npm",
+      icon: "NPM",
+    },
+  ],
+};
+
+module.exports = techStack;
